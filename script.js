3bd791a6f4a24ffdc046c7f04f9b747003d61b6c
Justin Lau <jlau11150@gmail.com>
2026-08-10 20:08:01 -0700

    Completed steps 3, 4 and 5 of the project, forgot to commit each one!

--- script.js
+++ script.js
@@ -16,9 +16,60 @@ function getComputerChoice() {
             result = "scissors";
             break;
     }
+
+    return result;
     
     // console.log(result)
 }
 
-// Call function area
-// getComputerChoice()
+// Step 3: Write the logic to get the human choice
+function getHumanChoice() {
+    let response = prompt("Choose you're action!")
+    // console.log(response);
+    return response;
+}
+
+
+// Step 4: Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+
+// Step 5: Write the logic to play a single round
+function playRound(humanChoice, computerChoice) {
+    // LOSER OPTIONS 
+    console.log(`Human parameter is ${humanChoice}`)
+    console.log(`Computer parameter is ${computerChoice}`)
+    if (humanChoice.toLowerCase() === 'rock' && computerChoice === 'paper') {
+        console.log("YOU LOSE! Paper beats rock :(");
+        computerScore += 1;
+    } else if (humanChoice.toLowerCase() === 'paper' && computerChoice === 'scissors') {
+        console.log("YOU LOSE! Scissors beats paper :(");
+        computerScore += 1;
+    } else if (humanChoice.toLowerCase() === 'scissors' && computerChoice === 'rock'){
+        console.log('YOU LOSE! Rock beats scissors :)');
+        computerScore += 1;
+    } else if((humanChoice.toLowerCase() && computerChoice === 'rock') || (humanChoice.toLowerCase() && computerChoice === 'paper') || (humanChoice.toLowerCase() && computerChoice === 'scissors')) {
+        console.log("A TIE! Nobody wins.")
+    } else {
+        console.log("YOU WIN! Nice Job :) ");
+        humanScore += 1;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+playRound(humanSelection, computerSelection)
+
+
+
+/* just me messing around 
+
+try{
+        let human_response = window.prompt("Choose you're action!");
+    } catch(error) {
+        console.log("We couldn't get the user to prompt")
+    }
+*/
